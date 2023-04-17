bab71a6267ae3ac0f85d9972252f830a4bbdeac2
bass9030 <bass90301.naver.comm@gmail.com>
2023-04-17 19:01:17 +0900

    edit: 에디터 테마 적용

--- app/src/main/assets/CodeMirror/index.mjs
+++ app/src/main/assets/CodeMirror/index.mjs
@@ -1,16 +1,23 @@
 import { basicSetup } from "codemirror"
-import {javascript} from "@codemirror/lang-javascript"
+import { javascript, javascriptLanguage } from "@codemirror/lang-javascript"
 import { keymap, EditorView } from "@codemirror/view"
 import { indentWithTab } from "@codemirror/commands"
 import { vscodeKeymap } from "@replit/codemirror-vscode-keymap"
-import { materialDark } from '@ddietr/codemirror-themes/material-dark'
+import * as thememirror from "thememirror"
+
+//TODO: autocomplete 적용
 
 let view = new EditorView({
     extensions: [
         basicSetup,
         javascript(),
         keymap.of(indentWithTab, vscodeKeymap),
-        materialDark,
+        thememirror.dracula,
     ],
     parent: document.body
 });
+
+const resize = () => document.getElementsByClassName('cm-editor')[0].setAttribute("style", `height: ${window.innerHeight}px;`);
+
+addEventListener('load', resize);
+addEventListener('resize', resize);
